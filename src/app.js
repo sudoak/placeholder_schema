158c2bafad281c5b0b6782f52552fa37e7540b75
sudoak <akram@coffeebeans.io>
2020-01-17 11:35:20 +0700

    [added] - low db support

--- src/app.js
+++ src/app.js
@@ -1,8 +1,33 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
 const port = 3000
 
+app.use(bodyParser.json());
+
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/schema', (req,res) => {
+    db.get('schemas')
+    .push(req.body)
+    .write()
+    res.status(200).json({err:null, message:"successfully stored", code: 200, data : req.body});
+});
+
+app.get('/schema/:id', async (req,res) => {
+    const schmemaId = req.params.id || 1;
+    const _ = await db.get('schemas')
+    .find({ id : parseInt(schmemaId) })
+    .value();
+    res.status(200).json(_);
+});
+
 
 app.get('/schema/v1', (req,res)=>{
     const schema = {
