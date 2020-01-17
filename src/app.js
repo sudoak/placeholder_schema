5d604dba6abade4bde37ad9d8af7279f0a30f91f
sudoak <akram@coffeebeans.io>
2020-01-17 13:46:50 +0700

    [added] - cors

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
-const port = 3000
+const cors = require('cors');
+const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
-
+app.use(cors());
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 
